2f7900565cb0b63597682c7ad2ce17167e2ae353
Feelone <thinnarat.j@ku.th>
2024-03-06 23:12:18 +0700

    Change Write Wish Button

--- resources/js/Layouts/DecorateCustom.jsx
+++ resources/js/Layouts/DecorateCustom.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import WishPreview from "@/Components/Card/WishPreview";
+import PrimaryButton from "@/Components/PrimaryButton";
 
 export default function DecorateCustom(props) {
     const [template, setTemplate] = useState('Standard Card');
@@ -21,6 +22,14 @@ export default function DecorateCustom(props) {
                     content={props.customization.content}
                     decorate={props.customization.decorate}
                     />
+            </div>
+            <div className='w-full flex justify-center'>
+                <PrimaryButton className='flex justify-center items-center bg-[#005555]'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                    </svg>
+                    <span>เขียน</span>
+                </PrimaryButton>
             </div>
         </div>
     )
