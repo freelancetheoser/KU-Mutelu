b1f42effcc5443189a2c1cb002fe894b2979d1ce
Feelone <thinnarat.j@ku.th>
2024-04-17 10:28:47 +0700

    Update PostDetail.jsx

--- resources/js/Components/PostDetail.jsx
+++ resources/js/Components/PostDetail.jsx
@@ -63,26 +63,35 @@ export default function PostDetail ({image, content, user, postId, comments, fee
                 <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
                 <img src={image} alt="" className='rounded'/><br />
                 {content}
-                </Dialog.Description>
-                {renderedComments}
-                <fieldset className="mb-[15px] flex items-center gap-5">
-                <input
-                    className="placeholder-teal-500  text-violet11 shadow-teal-500 focus:shadow-teal-500 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
-                    id="content"
-                    type="text"
-                    name="content"
-                    value={data.content}
-                    placeholder='เขียนเพื่อแสดงความคิดเห็น'
-                    onChange={(e) => setData('content', e.target.value)}
-                />
-                </fieldset>
-                <div className="mt-[25px] flex justify-center">
-                <Dialog.Close asChild>
-                    <button onClick={handleSubmit} className="bg-gradient-to-r from-[#005555] to-[#069A8E] text-white inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
-                    แสดงความคิดเห็น
-                    </button>
-                </Dialog.Close>
+                <div className="flex overflow-auto">
+                    <div className='w-full max-h-24 m-2 flex-shrink-0 '>
+                        {renderedComments}
+                    </div>
                 </div>
+                </Dialog.Description>
+                <div className='flex justify-center item-center space-x-4'>
+                    <fieldset className="mb-[15px] flex items-center gap-5 w-3/4">
+                    <input
+                        className="placeholder-teal-500  text-violet11 shadow-teal-500 focus:shadow-teal-500 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+                        id="content"
+                        type="text"
+                        name="content"
+                        value={data.content}
+                        placeholder='เขียนเพื่อแสดงความคิดเห็น'
+                        onChange={(e) => setData('content', e.target.value)}
+                    />
+                    </fieldset>
+
+                    <Dialog.Close asChild>
+                        <button onClick={handleSubmit} className="bg-gradient-to-r from-[#005555] to-[#069A8E] text-white inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
+                                <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
+                            </svg>
+                        </button>
+                    </Dialog.Close>
+
+                </div>
+
                 <Dialog.Close asChild>
                 <button
                     className="hover:bg-gradient-to-r from-[#005555] to-[#069A8E] hover:text-white focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
